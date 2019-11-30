e9fe354e44836e412c169f214bebdd52b33e2eb6
刘苏豫 <2539282842@qq.co.com>
2019-11-30 18:19:56 +0800

    添加登录组件

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import router from './router' // 导入路由依赖
+import ElementUI from 'element-ui' //导入elementui依赖
+import 'element-ui/lib/theme-chalk/index.css'
+
+
+// 在Vue中全局使用
+Vue.use(ElementUI) // 使用element-ui
+Vue.use(router)  // 使用路由
+
 
 Vue.config.productionTip = false
 
